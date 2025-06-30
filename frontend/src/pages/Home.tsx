d1c817bd4f6b0c9ea2a9cf067b42dc40986a3f31
Maeva RODRIGUES <maeva.rodrigues@lapiscine.pro>
2025-06-30 10:55:51 +0200

    ➕ Adding Dependency:  Add tailwind + daisy ui

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,10 @@
 export function HomePage() {
-  return <p>You should start working here!</p>;
+  return<div className="p-4">
+  <h1 className="text-3xl font-bold text-center text-primary">Hello Daisy !</h1>
+  <button className="btn btn-primary mt-4">btn-primary</button>
+  <button className="btn btn-secondary mt-2">btn-secondary</button>
+  <button className="btn btn-accent mt-2">btn-accent</button>
+  <button className="btn btn-error mt-2">btn-error</button>
+</div>
 }
+
